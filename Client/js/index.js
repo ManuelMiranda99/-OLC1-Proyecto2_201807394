9019746b1765898df0df945982c118b85afb101c
Manuel Miranda <3057476220301@ingenieria.usac.edu.gt>
2020-05-12 11:43:17 -0600

    Abrir archivo
    Abrir archivo en pestaña

--- Client/js/index.js
+++ Client/js/index.js
@@ -17,36 +17,9 @@ function setActualTab(tab){
     actualEditor = copyEditorList[tab];
 }
 
-var tabList = new Array();
-function linkedList(tab, name){
-    var obj = new Object();
-    obj.tab = tab;
-    obj.name = name;
-}
-
-function changeTab(tabs, tab){
-    var id = tab.replace('Nueva', '');
-    setActualTab('copyCode' + id);
-
-    var tab1 = document.getElementById(tab);
-    var listOfTabs = document.getElementById(tabs);
-    var cTab = document.getElementById('c' + tab);
-    var listCTabs = document.getElementById('copyContent' + tabs);
-
-    var i=0;
-    while(typeof listCTabs.getElementsByTagName('div')[i] != 'undefined'){
-        $(document).ready(function(){
-            $(listCTabs.getElementsByTagName('div')[i]).css('display', 'none');
-            $(listCTabs.getElementsByTagName('li')[i]).css('background', '');
-            $(listCTabs.getElementsByTagName('li')[i]).css('padding-bottom', '');
-        });
-        i++;
-    }
-}
-
 function newTab(){    
 
-    $('<li class="nav-item copyTabDefault"><a href="#tab'+countOfTabs+'" data-toggle="tab" class="nav-link copyTabDefault" onclick="setActualTab(' + countOfTabs + ')">Tab ' + countOfTabs + '</a></li>').appendTo('#tabsS');    
+    $('<li class="nav-item copyTabDefault"><a href="#tab'+countOfTabs+'" data-toggle="tab" class="nav-link copyTabDefault" onclick="setActualTab(' + countOfTabs + ')" id="selectTab' + countOfTabs + '">Tab ' + countOfTabs + '</a></li>').appendTo('#tabsS');    
 
     $('<div class="tab-pane" id="tab' + countOfTabs + '"style="font-size: 17px;width: 100%;height: 277px;"><div class="list-group Editor" id="tab' + countOfTabs + '"></div></div>').appendTo('.tab-content');
 
@@ -62,6 +35,17 @@ function newTab(){
     setActualTab(countOfTabs)
 
     countOfTabs++;
+}
+
+function openFile(files){
+    var file = files[0];
+    var reader = new FileReader();
+    reader.onload = function(e){
+        newTab();
+        actualEditor.setValue(e.target.result);
+    };
+    reader.readAsText(file);
+    file.clear;
 }
 
 function compareCode(){    
